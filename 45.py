91bd98f88c7e64ece382efa292cff33595946b0b
AMSdani <130896947+evddaniil@users.noreply.github.com>
2024-10-26 22:21:21 +0300

    Add files via upload

---
+++ 45.py
@@ -0,0 +1,11 @@
+d = int(input('день '))
+m = input('месяц ')
+
+if m == "Январь" and d == 1:
+ print("Новый год")
+elif m == "Июль" and d == 1:
+ print("День Канады")
+elif m == "Декабрь" and d == 25:
+ print("Рождество")
+else:
+ print("ошибка")
